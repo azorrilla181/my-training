caa7135d1a4b29ca16c3e15483bb0dd0b539b262
Aaron Zorrilla <153756600+azorrilla181@users.noreply.github.com>
2025-11-12 13:41:43 -0700

    Enhance comments for API procedures clarity
    Added comments to clarify the purpose of procedures in the API.

--- src/server/api/tasks/create-task/create-task.ts
+++ src/server/api/tasks/create-task/create-task.ts
@@ -1,3 +1,10 @@
+// Remember that procedures are single API Endpoints
+// To break them down, they are callable functions in the router with a 
+//   I/O contract (input and output)
+//   handlers or all the logic it needs for the CRUD to work
+//   and even though optional, middleware is important for security since we don't want every user to see everything or has access to everything in the app
+//   path
+
 import { z } from 'zod/v4';
 import { prisma } from '../../../../../prisma/client';
 import { authorizedProcedure } from '../../trpc';
